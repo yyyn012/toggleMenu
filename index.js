85fb5a3bb7c5a227a38e6f8234ddd5d11c08f4cd
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-04-06 17:00:02 +0900

    class name change

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
+const navBtn = document.querySelector(".nav-btn");
 const nav = document.querySelector(".nav");
-const menu = document.querySelector(".menu");
-const dropContent = document.querySelectorAll(".drop-content .sub-list");
-const dropBtn = document.querySelector(".drop-btn");
 
 function openMenu() {
+  navBtn.classList.toggle("active");
   nav.classList.toggle("active");
-  menu.classList.toggle("active");
 }
-
-function optionOpen() {
-  dropContent.classList.toggle("on");
-}
-
-dropContent.addEventListener("click", optionOpen);
